687c41fc517703ccf1deeebe93344b7a9b42c432
MrKouch <guykou@post.bgu.ac.il>
2024-12-25 22:58:56 +0000

    MessageBus and MicroService no concurrency

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,5 +1,10 @@
 package bgu.spl.mics;
-import java.util.*;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Enumeration;
+import java.util.Iterator;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -7,70 +12,109 @@ import java.util.*;
  * All other methods and members you add the class must be private.
  */
 public class MessageBusImpl implements MessageBus {
-	
-	// Fields
-	private static volatile MessageBus theBus;
-	private HashMap<Event<?>, Queue<MicroService>> eventsMap;
-	private HashMap<Broadcast, Queue<MicroService>> broadcastsMap;
+    
+    // Fields
+    private static volatile MessageBus theBus;
+    private ConcurrentHashMap<Class<? extends Event<?>>, LinkedBlockingQueue<MicroService>> eventsSubscribers;
+    private ConcurrentHashMap<Class<? extends Broadcast>, LinkedBlockingQueue<MicroService>> broadcastsSubscribers;
+    private ConcurrentHashMap<MicroService, LinkedBlockingQueue<Message>> microServicesMessages;
+    private ConcurrentHashMap<Event<?>, Future<?>> eventsFutures;
 
-	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+    private MessageBusImpl() {
+        eventsSubscribers = new ConcurrentHashMap<>();
+        broadcastsSubscribers = new ConcurrentHashMap<>();
+    }
 
-	}
+    @Override
+    public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+        eventsSubscribers.putIfAbsent(type, new LinkedBlockingQueue<MicroService>());
+        eventsSubscribers.get(type).add(m); // MAKE SURE: Can we assume that m is not in the queue right now?
+    }
 
-	@Override
-	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+    @Override
+    public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+        broadcastsSubscribers.putIfAbsent(type, new LinkedBlockingQueue<MicroService>());
+        broadcastsSubscribers.get(type).add(m); // MAKE SURE: Can we assume that m is not in the queue right now?
+    }
 
-	}
+    @Override
+    public <T> void complete(Event<T> e, T result) {
+        Future<T> future = (Future<T>) eventsFutures.get(e); // Handle this warning
+        future.resolve(result);
+        eventsFutures.remove(e);
+    }
 
-	@Override
-	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
+    @Override
+    public void sendBroadcast(Broadcast b) {
+        Iterator<MicroService> bSubscribers = broadcastsSubscribers.get(b.getClass()).iterator();
+        while (bSubscribers.hasNext())  
+            microServicesMessages.get(bSubscribers.next()).add(b);
+    }
 
-	}
+    
+    @Override
+    public <T> Future<T> sendEvent(Event<T> e) {
+        MicroService m = eventsSubscribers.get(e.getClass()).remove();
+        microServicesMessages.get(m).add(e);
+        eventsSubscribers.get(e.getClass()).add(m);
 
-	@Override
-	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
+        Future<T> future = new Future<T>();
+        eventsFutures.put(e, future);
+        return future; // MAKE SURE
+    }
 
-	}
+    @Override
+    public void register(MicroService m) {
+        microServicesMessages.put(m, new LinkedBlockingQueue<Message>());
+    }
 
-	
-	@Override
-	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void unregister(MicroService m) {
+        microServicesMessages.remove(m);
 
-	@Override
-	public void register(MicroService m) {
-		// TODO Auto-generated method stub
+        Enumeration<Class<? extends Event<?>>> enu = eventsSubscribers.keys();
+        // Displaying the Enumeration 
+        while (enu.hasMoreElements()) { 
+            System.out.println(enu.nextElement()); 
+        } 
 
-	}
+        // Iterator<ConcurrentHashMap.Entry<Class<? extends Event<?>>, LinkedBlockingQueue<MicroService>>>itr =
+        //  eventsSubscribers.entrySet().iterator();
+        // while (itr.hasNext()) {
+        //     ConcurrentHashMap.Entry<Class<? extends Event<?>>, LinkedBlockingQueue<MicroService>> entry
+        //         = itr.next();
+        //  eventsSubscribers.get(entry).remove(entry);
+        // }
+    }
 
-	@Override
-	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
+    @Override
+    public Message awaitMessage(MicroService m) throws InterruptedException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    // getters
+    // 
+    public static MessageBus getMessageBus() {
+        if(theBus == null) {
+            synchronized(MessageBus.class) {
+                if(theBus == null)
+                theBus = new MessageBusImpl();
+            }
+        }
+        return theBus;
+    }
+    public static MessageBus removeFromQueue() {
+        if(theBus == null) {
+            synchronized(MessageBus.class) {
+                if(theBus == null)
+                theBus = new MessageBusImpl();
+            }
+        }
+        return theBus;
+    }
 
-	@Override
-	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	// getters
-	// initializes the bus in the first call, and returns the existing one for all later calls.
-	public static MessageBus getMessageBus() {
-		if(theBus == null) {
-			synchronized(MessageBus.class) {
-				if(theBus == null)
-				theBus = new MessageBusImpl();
-			}
-		}
-		return theBus;
-	}
 }
+
+
+
