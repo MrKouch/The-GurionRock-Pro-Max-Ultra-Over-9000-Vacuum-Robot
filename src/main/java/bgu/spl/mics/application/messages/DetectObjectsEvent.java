c538433c376ba71ce3fa486f917a30833fea1561
MrKouch <guykou@post.bgu.ac.il>
2024-12-27 15:17:11 +0000

    messeges classes created

---
+++ src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
@@ -0,0 +1,16 @@
+package bgu.spl.mics.application.messages;
+
+import bgu.spl.mics.Event;
+
+public class DetectObjectsEvent implements Event<DetectObjectsEvent> {
+//  Sent by: Camera
+// • Handled by: a LiDar Worker
+// • Details:
+//      o Includes DetectedObjects.
+//      o The Camera send DetectedObjectsEvent of the Objects with time T for all the
+//          subscribed Lidar workers to this event at time T, and one of them deals with a single
+//          event.
+
+    // Fields
+    
+}
